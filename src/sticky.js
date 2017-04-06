a10c3bcbecb8ae093241bc844e49261203b8c4d9
Yvain Liechti <yliechti@clever-age.com>
2017-04-06 16:29:40 +0200

    fix bug when use stickOrStuck on a Sticky object not totaly init because not enough space to set it sticky

--- src/sticky.js
+++ src/sticky.js
@@ -39,7 +39,6 @@
 				this.options = defaultOptions;
 
 				if (opt) {
-
 					this.options.stickClass = opt.stickClass || defaultOptions.stickClass;
 					this.options.stuckClass = opt.stuckClass || defaultOptions.stuckClass;
 					this.options.stuckLimitSelector = opt.stuckLimitSelector || defaultOptions.stuckLimitSelector;
@@ -75,26 +74,30 @@
 				this.isOffsetFunction = isFunction(this.options.offsetTop);
 
 				this.offsetTop = this.isOffsetFunction ? this.options.offsetTop() : this.options.offsetTop || 0;
+
+				this.initialized = false;
 				this.init();
 			}
 
 			Sticky.prototype.init = function init() {
+				if(this.initialized || this.stickyLimit.offsetHeight <= this.parent.offsetHeight){
+					return;
+				}
 
 				this.options.onInit(this.el);
 
-				if(this.stickyLimit.offsetHeight > this.parent.offsetHeight){
+				this.parent.checkVisibility = new CheckVisibility(this.parent);
 
-					this.parent.checkVisibility = new CheckVisibility(this.parent);
+				// init handler on ready
+				this.onScroll();
 
-					// init handler on ready
-					this.onScroll();
+				this.onScroll = this.onScroll.bind(this);
+				this.onResize = this.onResize.bind(this);
 
-					this.onScroll = this.onScroll.bind(this);
-					this.onResize = this.onResize.bind(this);
+				window.addEventListener('scroll', this.onScroll);
+				window.addEventListener('resize', this.onResize);
 
-					window.addEventListener('scroll', this.onScroll);
-					window.addEventListener('resize', this.onResize);
-				}
+				this.initialized = true;
 			};
 
 			Sticky.prototype.onScroll = function onScroll() {
